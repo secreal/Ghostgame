22858efa9756c07e833525ae23497e4199fb000a
git <git@git.com>
2013-08-14 17:23:54 +0700

    rubah
    Signed-off-by: git <git@git.com>

--- master/primary.cmd
+++ master/primary.cmd
@@ -1,5 +1,5 @@
 @echo off
 
 :copymap
-secrael "C:\program Files\Warcraft III\Maps\Download" "C:\Dropbox\Maps\Download" /PURGE /ZB /S /E /V /R:0 /W:1
+secrael "C:\program Files\Warcraft III\Maps\Download" "C:\Dropbox\Maps\Download" /PURGE /ZB /S /E /V /R:0 /W:2
 
